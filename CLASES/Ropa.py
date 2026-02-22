79a327203cce0ca7e368b1f19ac47c5c8e883d21
max7cv <kevinstiv2005@gmail.com>
2026-02-22 11:31:14 -0500

    feat: se agregaron docstring faltantes y valores de retorno

--- CLASES/Ropa.py
+++ CLASES/Ropa.py
@@ -1,13 +1,33 @@
 from CLASES.Producto import Producto
+
+
 class Ropa(Producto):
-    def __init__(self,codigo:str,nombre:str,  marca: str, precio: float, stock: int, tipo: str, talla: str, color: str)-> None:
-        """
-        """
+    def __init__(
+        self,
+        codigo: str,
+        nombre: str,
+        marca: str,
+        precio: float,
+        stock: int,
+        tipo: str,
+        talla: str,
+        color: str,
+    ) -> None:
+        """:param codigo: Código único del producto.
+        :param nombre: Nombre del producto.
+        :param marca: Marca del producto.
+        :param precio: Precio del producto.
+        :param stock: Cantidad disponible en inventario.
+        :param tipo: Tipo de prenda (ej: Camisa, Pantalón, Vestido).
+        :param talla: Talla de la prenda (ej: XS, S, M, L, XL).
+        :param color: Color de la prenda (ej: Azul, Rojo, Negro).
+        :return: None"""
+
         super().__init__(codigo, nombre, marca, precio, stock)
         self.tipo: str = tipo
         self.talla: str = talla
         self.color: str = color
-    
+
     def obtener_info(self) -> str:
         """
         Devuelve una representación formateada con la información
@@ -28,10 +48,7 @@ class Ropa(Producto):
             ("TALLA", self.talla),
             ("COLOR", self.color),
         ]
+
     def __str__(self) -> str:
-        """Devuelve la representación en texto del objeto Ropa.
-        """
+        """Devuelve la representación en texto del objeto Ropa."""
         return self.obtener_info()
-    
-
-
